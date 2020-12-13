07420b974b2938a57409eec478a75bc28ee598b0
Haider <haider.haider168@gmail.com>
2020-12-13 13:44:39 +0100

    header desktop/tablet view fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import './header.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBook,
+    faBriefcase,
+    faAddressCard
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
 
@@ -14,6 +20,11 @@ export default function Header() {
                     <div className="bar2"></div>
                     <div className="bar3"></div>
                 </div>
+                <ul className="menu__list-large">
+                    <li className="menu__list-item"><a href="#skills"><FontAwesomeIcon icon={faBook}></FontAwesomeIcon> Skills</a></li>
+                    <li className="menu__list-item"><a href="#work"><FontAwesomeIcon icon={faBriefcase}></FontAwesomeIcon> Recent work</a></li>
+                    <li className="menu__list-item"><a href="#contact"><FontAwesomeIcon icon={faAddressCard}></FontAwesomeIcon> Contact</a></li>
+                </ul>
             </div>
             <div>
                 <ul className={"menu__list" + (isMenuOpen ? ' show' : '')}>
