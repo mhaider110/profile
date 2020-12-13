7f3398f2f1f152e1c3ce1f4a4e8b4d2de555f44f
Haider <haider.haider168@gmail.com>
2020-12-13 16:05:36 +0100

    remove used resource

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-
-import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
 import Footer from './components/footer/footer'
